4fe5a761c56c07269df678697dee13a10f5751f1
arvind8998 <asangwan8998@gmail.com>
2021-07-03 16:10:13 +0530

    Added zig zag order traversal

--- GenericTrees/GenericTree.java
+++ GenericTrees/GenericTree.java
@@ -114,28 +114,28 @@ public class GenericTree {
         return smallAns;
     }
 
-    public static ArrayList<Integer> __nodeToRootPath(Node node, int data){
-        if(node.data == data){
+    public static ArrayList<Integer> __nodeToRootPath(Node node, int data) {
+        if (node.data == data) {
             ArrayList<Integer> base = new ArrayList<Integer>();
             base.add(node.data);
             return base;
         }
         ArrayList<Integer> ans = new ArrayList<>();
-        for(Node child: node.children){
-           ArrayList<Integer> recAns = __nodeToRootPath(child,data);
-           if(recAns.size() >0){
-              for(int a: recAns){
-                  ans.add(a);
-              }
-              break ;   
-           }
-            
+        for (Node child : node.children) {
+            ArrayList<Integer> recAns = __nodeToRootPath(child, data);
+            if (recAns.size() > 0) {
+                for (int a : recAns) {
+                    ans.add(a);
+                }
+                break;
+            }
+
         }
-        if(ans.size() > 0){
-             ans.add(node.data);
+        if (ans.size() > 0) {
+            ans.add(node.data);
         }
-       
-       return ans;
+
+        return ans;
     }
 
     public static int distanceBetweenNodes(Node node, int d1, int d2) {
@@ -248,56 +248,56 @@ public class GenericTree {
         System.out.print(".");
     }
 
-    public static Node getTail(Node node){
-        while(node.children.size() !=0){
+    public static Node getTail(Node node) {
+        while (node.children.size() != 0) {
             node = node.children.get(0);
         }
         return node;
     }
-    
-      public static void linearize(Node node){
-        for(Node child: node.children){
+
+    public static void linearize(Node node) {
+        for (Node child : node.children) {
             linearize(child);
         }
-        for(int i=node.children.size()-1; i>0; i--){
-            Node tail = getTail(node.children.get(i-1));
+        for (int i = node.children.size() - 1; i > 0; i--) {
+            Node tail = getTail(node.children.get(i - 1));
             tail.children.add(node.children.get(i));
-            node.children.remove(i); 
+            node.children.remove(i);
         }
-      }
+    }
 
-      public static ArrayList<Integer> ___nodeToRootPath(Node node, int data) {
+    public static ArrayList<Integer> ___nodeToRootPath(Node node, int data) {
         if (node.data == data) {
-          ArrayList<Integer> path = new ArrayList<>();
-          path.add(node.data);
-          return path;
+            ArrayList<Integer> path = new ArrayList<>();
+            path.add(node.data);
+            return path;
         }
-    
+
         for (Node child : node.children) {
-          ArrayList<Integer> ptc = ___nodeToRootPath(child, data);
-          if (ptc.size() > 0) {
-            ptc.add(node.data);
-            return ptc;
-          }
+            ArrayList<Integer> ptc = ___nodeToRootPath(child, data);
+            if (ptc.size() > 0) {
+                ptc.add(node.data);
+                return ptc;
+            }
         }
-    
+
         return new ArrayList<>();
-      }
-    
-      public static int lca(Node node, int d1, int d2) {
+    }
+
+    public static int lca(Node node, int d1, int d2) {
         ArrayList<Integer> path1 = ___nodeToRootPath(node, d1);
         ArrayList<Integer> path2 = ___nodeToRootPath(node, d2);
-        int l1 = path1.size()-1, l2 =path2.size()-1, potentialLca = -1;
-        while(l1  >= 0 && l2 >= 0){
-            if(path1.get(l1) != path2.get(l2))
+        int l1 = path1.size() - 1, l2 = path2.size() - 1, potentialLca = -1;
+        while (l1 >= 0 && l2 >= 0) {
+            if (path1.get(l1) != path2.get(l2))
                 break;
-                potentialLca = path1.get(l1);
-                l1--;
-                l2--;
-            
+            potentialLca = path1.get(l1);
+            l1--;
+            l2--;
+
         }
         return potentialLca;
-      }
+    }
 
     public static void levelOrderLinewise(Node node) {
         LinkedList<Node> queue = new LinkedList<>();
@@ -322,39 +322,31 @@ public class GenericTree {
         System.out.println();
     }
 
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
+    public static void levelOrderLinewiseZZ(Node node){
+        LinkedList<Node> stack = new LinkedList<Node>();
+        LinkedList<Node> queue = new LinkedList<Node>();
+        queue.addLast(node);
         
-        LinkedList<Integer> queue = new LinkedList<>();
-        LinkedList<Integer> stack = new LinkedList<>();
-        queue.addLast(root);
         int level = 0;
-        
-        while(queue.size() > 0) {
+        while(queue.size() >0){
             int size = queue.size();
-            List<Integer> base = new ArrayList<>();
-            whike(size-- > 0){
-                TreeNode rn = queue.removeFirst();
-                base.add(rn.data);
-
-            if(level % 2 == 0){
-                for(Node child : root.children){
-                    stack.addFirst(child.data);
-                }
+            while(size -- >0){
+               Node removedLast = queue.removeFirst();
+               System.out.print(removedLast.data + " ");
+               if(level % 2 == 0){
+                   for(Node child : removedLast.children)
+                   stack.addFirst(child);
+               }
+               else{
+                   for(int i= removedLast.children.size()-1; i >=0; i--){
+                       stack.addFirst(removedLast.children.get(i));
+                   }
+               }
             }
-            else{
-                for(int i = rn.children.size()-1; i>=0; i--){
-                    Node child = rn.children.get(i);
-                    stack.addFirst(child);
-                }
-            }
-            
+            level++;
+            LinkedList<Node> temp = queue;
+            queue = stack;
+            stack = temp;
+            System.out.println();
         }
-        
-    }
-    level++;
-    ans.add(base);
-    LinkedList<Node> temp = queue;
-    
-}
+      }
