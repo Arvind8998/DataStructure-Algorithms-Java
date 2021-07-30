900362b3ccd530e5534e816df1d848cd0076dc8c
arvind <arvind@clevertap.com>
2021-07-30 16:31:23 +0530

    Added 1-9 target sum combination sum problem

---
+++ LevelUp/LeetCodeSubmissions/LeetCode216.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void getCombinations(List<List<Integer>> ans, List<Integer> combinations, int k, int target, int idx){
+        if(target == 0 && combinations.size() == k){
+            ans.add(new ArrayList<>(combinations));
+            return;
+        }
+        
+        for(int i=idx; i<=9; i++){
+            if(target-i >= 0){
+                combinations.add(i);
+                getCombinations(ans, combinations, k, target-i, i+1);
+                combinations.remove(combinations.size()-1);
+            }
+        }
+    }
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> combinations = new ArrayList<>();
+        getCombinations(ans, combinations, k, n, 1);
+        return ans;
+    }
+}
