2ffa04c3e1cc0b6a505bebb20d1cca3be23897d8
arvind <arvind@clevertap.com>
2021-08-01 23:17:01 +0530

    Added nQueen problem optimised

---
+++ LevelUp/LeetCodeSubmissions/LeetCode52.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean[] row, col, diag, adiag;
+
+    public int nQueen_01(int n, int m, int floor, int tnq, String asf, boolean[] row, boolean[] col, boolean[] diag, boolean[] adiag) {
+        if (tnq == 0) {
+            System.out.println(asf);
+            return 1;
+        }
+
+        int count = 0;
+        
+ for (int room = 0; room < m; room++) {
+            int r = floor, c = room;
+            if (!col[c] && !diag[r + c] && !adiag[r - c + m - 1]) {
+                col[c] = diag[r + c] = adiag[r - c + m - 1] = true;
+                count += nQueen_01(n, m, floor + 1, tnq - 1, asf + "(" + r + "," + c + ") ", row, col, diag, adiag);
+                col[c] = diag[r + c] = adiag[r - c + m - 1] = false;
+            }
+        }
+
+        return count;
+    }
+    
+    public int totalNQueens(int n) {
+        row = new boolean[n];
+        col = new boolean[n];
+        diag = new boolean[n + n - 1];
+        adiag = new boolean[n + n - 1];
+        boolean[][] box = new boolean[n][n];
+        return nQueen_01(n, n, 0, n, "", row, col, diag, adiag);
+    }
+}
