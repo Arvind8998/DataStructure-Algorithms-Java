091db8b20ae0cf4e50b06e370ddc322785a7d0bd
arvind <arvind@clevertap.com>
2021-08-07 13:24:03 +0530

    Added nqueen problem

--- LevelUp/LeetCodeSubmissions/LeetCode52.java
+++ LevelUp/LeetCodeSubmissions/LeetCode52.java
@@ -1,20 +1,30 @@
 class Solution {
-    boolean[] row, col, diag, adiag;
+    int rowMask=0, colMask=0, diagMask=0, adiagMask=0;
 
-    public int nQueen_01(int n, int m, int floor, int tnq, String asf) {
+    public int getNQueensPosition(int n, int m, int floor, int tnq) {
         if (tnq == 0) {
-            System.out.println(asf);
             return 1;
         }
 
         int count = 0;
         
  for (int room = 0; room < m; room++) {
-            int r = floor, c = room;
-            if (!row[r] && !col[c] && !diag[r + c] && !adiag[r - c + m - 1]) {
-                row[r] = col[c] = diag[r + c] = adiag[r - c + m - 1] = true;
-                count += nQueen_01(n, m, floor + 1, tnq - 1, asf + "(" + r + "," + c + ") ");
-                row[r] = col[c] = diag[r + c] = adiag[r - c + m - 1] = false;
+            int row = floor, col = room;
+     
+            if ( 
+                (colMask & (1<< col)) == 0 &&
+                (diagMask & (1<< (row +col))) == 0  &&
+                (adiagMask & (1<< (row - col + m-1))) == 0 ) {
+                
+                colMask ^=  (1<<col);
+                diagMask ^=  (1<< (row+col));
+                adiagMask ^=  (1<< (row-col + m -1));
+                
+                count += getNQueensPosition(n, m, floor + 1, tnq - 1);
+                
+                colMask ^=  (1<<col);
+                diagMask ^=  (1<< (row+col));
+                adiagMask ^=  (1<< (row-col + m -1));
             }
         }
 
@@ -22,11 +32,6 @@ class Solution {
     }
     
     public int totalNQueens(int n) {
-        row = new boolean[n];
-        col = new boolean[n];
-        diag = new boolean[n + n - 1];
-        adiag = new boolean[n + n - 1];
-        boolean[][] box = new boolean[n][n];
-        return nQueen_01(n, n, 0, n, "");
+        return getNQueensPosition(n, n, 0, n);
     }
 }
