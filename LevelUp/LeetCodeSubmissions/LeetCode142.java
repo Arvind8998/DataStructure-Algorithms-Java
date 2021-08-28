68d99ed9b01de1c6692f339a80afb7854ad9f7be
Arvind Sangwan <asangwan8998@gmail.com>
2021-08-28 20:44:31 +0530

    Added leetcode 142 cycle start point

---
+++ LevelUp/LeetCodeSubmissions/LeetCode142.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode meetingPoint(ListNode head){
+        if(head == null || head.next == null) return null;
+        
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) return slow;
+        }
+        return null;
+    }
+    
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+
+        ListNode mp = meetingPoint(head);
+        if(mp == null) return mp;
+        
+        ListNode slow = head, fast = mp;
+        
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
