7e18707b79b57ee6e0700d3a0e138e13a6f99c6f
Arvind Sangwan <asangwan8998@gmail.com>
2021-10-27 20:28:12 +0530

    leetcode 62

--- LevelUp/LeetCodeSubmissions/LeetCode62.java
+++ LevelUp/LeetCodeSubmissions/LeetCode62.java
@@ -1,30 +1,31 @@
 class Solution {
-    public int mazePath(int sr, int sc, int er, int ec, int[][]dp, int[][] dir){
-        if(sr == er && sc == ec){
-            return dp[sr][sc] = 1;
-        }
-        if(dp[sr][sc] != 0){
-            return dp[sr][sc];
-        }
-
-        int count = 0;
-
-        for(int d=0; d<dir.length; d++){
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            
-            if(r >=0 && c >=0 && r <= er && c <= ec ){
-                count += mazePath(r, c, er, ec, dp, dir);
-            }
-        }
-        return dp[sr][sc] = count;
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        int[][] dir= { {1,0}, {0,1}};
+        
+        return mazePath__tabul(0,0, m-1, n-1, dp, dir);
     }
     
-    public int uniquePaths(int m, int n) {
-        int sr = 0, sc =0, er =m, ec= n;
-        int[][] dir = { {0,1}, {1,0}};
+    public static int mazePath__tabul(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir){
+        for(int sr=ER; sr>= SR; sr--){
+            for(int sc = EC; sc >= SC; sc--){
+                if(ER == sr && EC == sc ){
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+        int count = 0;
 
-        int[][] dp = new int[er+1][ec+1];
-        return mazePath(sr, sc, er-1, ec-1, dp, dir);
+        for(int[]d : dir){
+            int r = sr + d[0];
+            int c = sc + d[1];
+            if(r >=0 && c >=0 && r < dp.length && c < dp[0].length ){
+                count += dp[r][c];
+            }
+        }
+            
+        dp[sr][sc] = count;
+    }
+    }
+        return dp[SR][SC];
     }
 }
