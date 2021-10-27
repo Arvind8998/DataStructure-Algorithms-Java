5f3543e9a325f8b375381c15f66fc2e396b3ed7f
Arvind Sangwan <asangwan8998@gmail.com>
2021-10-27 20:34:53 +0530

    leetcode 63

--- LevelUp/LeetCodeSubmissions/LeetCode63.java
+++ LevelUp/LeetCodeSubmissions/LeetCode63.java
@@ -1,32 +1,33 @@
 class Solution {
-    public int mazePath(int sr, int sc, int er, int ec, int[][]dp, int[][] dir, int[][] obstacleGrid){
-        if(sr == er && sc == ec){
-            return dp[sr][sc] = 1;
-        }
-        if(dp[sr][sc] != 0){
-            return dp[sr][sc];
-        }
-
-        int count = 0;
-
-        for(int d=0; d<dir.length; d++){
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            
-            if(r >=0 && c >=0 && r <= er && c <= ec && obstacleGrid[r][c] == 0){
-                count += mazePath(r, c, er, ec, dp, dir, obstacleGrid);
-            }
-        }
-        return dp[sr][sc] = count;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
+       int[][] dp = new int[n][m];
+        int[][] dir= { {1,0}, {0,1}};
+        
+        return mazePath__tabul(0,0, n-1, m-1, dp, dir, obstacleGrid); 
     }
     
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int sr = 0, sc =0, er = obstacleGrid.length, ec= obstacleGrid[0].length;
-        int[][] dir = { {0,1}, {1,0}};
-        
-        if(obstacleGrid[0][0] == 1) return 0;
+      public static int mazePath__tabul(int SR, int SC, int ER, int EC, int[][] dp, int[][] dir, int[][] obstacleGrid){
+        for(int sr=ER; sr>= SR; sr--){
+            for(int sc = EC; sc >= SC; sc--){
+                if(ER == sr && EC == sc ){
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+        int count = 0;
 
-        int[][] dp = new int[er+1][ec+1];
-        return mazePath(sr, sc, er-1, ec-1, dp, dir, obstacleGrid);
+        for(int[]d : dir){
+            int r = sr + d[0];
+            int c = sc + d[1];
+            if(r >=0 && c >=0 && r < dp.length && c < dp[0].length && obstacleGrid[r][c] == 0){
+                count += dp[r][c];
+            }
+        }
+            
+        dp[sr][sc] = count;
+    }
+    }
+        return dp[SR][SC];
     }
 }
