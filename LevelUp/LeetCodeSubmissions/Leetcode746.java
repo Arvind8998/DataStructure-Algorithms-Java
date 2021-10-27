551653ded74d5b9c4e918ed146e1732d42371fb7
Arvind Sangwan <asangwan8998@gmail.com>
2021-10-27 23:25:17 +0530

    MinCostClimbbing stairs

---
+++ LevelUp/LeetCodeSubmissions/Leetcode746.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int N = cost.length;
+        int[] dp = new int[N];
+
+        for(int n=0; n<N; n++){
+            if(n <= 1){
+                dp[n] = cost[n];
+            }
+            else{
+                dp[n] = Math.min(dp[n-1], dp[n-2]) + cost[n];
+            }
+        }
+        return Math.min(dp[N-1], dp[N-2]);
+    }
+}
