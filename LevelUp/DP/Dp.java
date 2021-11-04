a5be2887944fed59b3e3ca99ab4b7430e6255bb8
Arvind Sangwan <asangwan8998@gmail.com>
2021-11-04 18:39:08 +0530

    Addded decode ways leetcode 91

--- LevelUp/DP/Dp.java
+++ LevelUp/DP/Dp.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Dp{
 
     public static void display(int[] dp){
@@ -188,7 +189,33 @@ public class Dp{
             for(int d=1; d<= 6 && sp + d <=ep; d++){
                 count += dp[sp+d];
             }
-            return dp[SP];
+            dp[sp] = count;
+        }
+        return dp[SP];
+    }
+
+        public int numDecoding(String str, int idx, int[]dp){
+            if(idx == str.length()) return dp[idx] = 1;
+            if(dp[idx] != -1) return dp[idx];
+            
+            char ch = str.charAt(idx);
+            if(ch == '0') return dp[idx] = 0;
+            int count = numDecoding(str, idx+1, dp);
+    
+            if(idx < str.length()-1){
+                char ch2 = str.charAt(idx+1);
+                int num = (ch -'0')*10 + (ch2 - '0');
+    
+                if(num <=26)
+                 count += numDecoding(str, idx+2, dp);
+            }
+            return dp[idx] = count;
+    
+        public int numDecodings(String str){
+            int n = str.length();
+            int[] dp = new int[n+1];
+            Arrays.fill(dp , -1);
+            return numDecoding(str,0, dp);
         }
     }
 
