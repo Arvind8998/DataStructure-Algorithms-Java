74c68be371b2c41f51b027dc0b668576583f8cd2
Arvind Sangwan <asangwan8998@gmail.com>
2021-11-04 18:40:09 +0530

    Leetcode 90

--- LevelUp/LeetCodeSubmissions/LeetCode91.java
+++ LevelUp/LeetCodeSubmissions/LeetCode91.java
@@ -1,29 +1,26 @@
 class Solution {
-    public static int getDecodingsCount(String str, int idx, int[] dp){
-        if(idx == str.length()) return dp[idx] =1;
-        
-        char ch = str.charAt(idx);
-        
+    public int numDecoding(String str, int idx, int[]dp){
+        if(idx == str.length()) return dp[idx] = 1;
         if(dp[idx] != -1) return dp[idx];
         
-        if(ch == '0'){
-            return dp[idx] = 0;
-        }
-        int count = getDecodingsCount(str, idx+1, dp);
+        char ch = str.charAt(idx);
+        if(ch == '0') return dp[idx] = 0;
+        int count = numDecoding(str, idx+1, dp);
 
-        if(idx < str.length() -1){
-            char ch1 = str.charAt(idx+1);
-            int num = (ch - '0')*10 + (ch1 -'0');
-            if(num <=26){
-                count += getDecodingsCount(str, idx+2, dp);
-            }
+        if(idx < str.length()-1){
+            char ch2 = str.charAt(idx+1);
+            int num = (ch -'0')*10 + (ch2 - '0');
+
+            if(num <=26)
+             count += numDecoding(str, idx+2, dp);
         }
-        
         return dp[idx] = count;
     }
-    public int numDecodings(String s) {
-        int[] dp = new int[s.length() +1];
-        Arrays.fill(dp, -1);
-        return getDecodingsCount(s, 0, dp);
+
+    public int numDecodings(String str){
+        int n = str.length();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp , -1);
+        return numDecoding(str,0, dp);
     }
 }
