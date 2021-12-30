d9052c9bba7478824ea953cff262ecad2cb2499e
Arvind Sangwan <asangwan8998@gmail.com>
2021-12-30 15:05:48 -0500

    Added cost problem

--- LevelUp/DP/EditDistance-Leetcode-72.java
+++ LevelUp/DP/EditDistance-Leetcode-72.java
@@ -27,3 +27,34 @@ class Solution {
         return minDistance(word1, word2, n, m, dp);
     }
 }
+
+// public int minDistance_02(String word1, String word2, int n, int m, int[]
+// cost, int[][] dp) {
+// if (n == 0 || m == 0) {
+// return dp[n][m] = (n == 0 ? m * cost[0] : n * cost[2]);
+// }
+
+// if (dp[n][m] != -1)
+// return dp[n][m];
+
+// int insert = minDistance_02(word1, word2, n, m - 1, cost, dp);
+// int delete = minDistance_02(word1, word2, n - 1, m, cost, dp);
+// int replace = minDistance_02(word1, word2, n - 1, m - 1, cost, dp);
+
+// if (word1.charAt(n - 1) == word2.charAt(m - 1))
+// return dp[n][m] = replace;
+// else
+// return dp[n][m] = Math.min(Math.min(insert + cost[0], delete + cost[2]),
+// replace + cost[1]);
+// }
+
+// public int minDistance(String s, String t) {
+// int n = s.length(), m = t.length();
+// int[][] dp = new int[n + 1][m + 1];
+// for (int[] d : dp)
+// Arrays.fill(d, -1);
+
+// int ans = minDistance(s, t, n, m, dp);
+
+// return ans;
+// }
