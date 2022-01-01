5257483cbc97a14a1cb258f05dc3606115049d37
Arvind Sangwan <asangwan8998@gmail.com>
2022-01-01 12:46:24 -0500

    Added longest bitonic sequence

---
+++ LevelUp/DP/LongestDecreasingSubsequenceGFG.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+// https://www.geeksforgeeks.org/longest-decreasing-subsequence/
+class Solution {
+
+    public int LIS_LR(int[] nums, int[] dp) {
+        int n = nums.length, maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+            maxLength = Math.max(dp[i], maxLength);
+        }
+        return maxLength;
+    }
+
+    // starting at biggest no start at lds-> LONGEST DECREASING SUBSEQUENCE
+    public int LIS_RL(int[] nums, int[] dp) {
+        int n = nums.length, maxLength = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+
+            }
+            maxLength = Math.max(dp[i], maxLength);
+        }
+        return maxLength;
+    }
+
+    public int LongestBitonicSequence(int[] nums) {
+        int n = nums.length;
+        int[] LIS = new int[n];
+        int[] LDS = new int[n];
+
+        LIS_LR(nums, LIS);
+        LIS_RL(nums, LDS);
+
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxLen = Math.max(maxLen, LIS[i] + LDS[i] - 1);
+        }
+        return maxLen;
+    }
+
+}
