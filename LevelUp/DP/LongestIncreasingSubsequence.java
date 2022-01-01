9a58664f2ae5c5cec8d97a76c22f517c40a71223
Arvind Sangwan <asangwan8998@gmail.com>
2022-01-01 12:08:56 -0500

    Added recursive solution

--- LevelUp/DP/LongestIncreasingSubsequence.java
+++ LevelUp/DP/LongestIncreasingSubsequence.java
@@ -20,4 +20,29 @@ class Solution {
         int[] dp = new int[n];
         return lengthOfLIS(nums, dp);
     }
+
+    // public static int LIS_memo(int[] arr, int ei, int[] dp) {
+    // if (dp[ei] != 0)
+    // return dp[ei];
+    // int maxLen = 1;
+    // for (int i = ei - 1; i >= 0; i--) {
+    // if (arr[i] < arr[ei]) {
+    // int recLen = LIS_memo(arr, i, dp);
+    // maxLen = Math.max(maxLen, recLen + 1);
+    // }
+    // }
+
+    // return dp[ei] = maxLen;
+    // }
+
+    // public static int LIS_Rec() {
+    // int[] arr = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15, 14 };
+    // int[] dp = new int[arr.length];
+    // int maxLen = 0;
+    // for (int i = 0; i < arr.length; i++) {
+    // maxLen = Math.max(maxLen, LIS_memo(arr, i, dp));
+    // }
+
+    // return maxLen;
+    // }
 }
