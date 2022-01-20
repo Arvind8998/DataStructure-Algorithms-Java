f01c180df144f69b1a826cf87994f8855889a202
Arvind Sangwan <asangwan8998@gmail.com>
2022-01-20 09:57:58 -0500

    Update 1LeetTwoSum.java

--- Foundation/LeetCodeSubmission/1LeetTwoSum.java
+++ Foundation/LeetCodeSubmission/1LeetTwoSum.java
@@ -1,19 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       ArrayList<Integer> ans = new ArrayList<Integer>();
+        int[] result = new int[2];
         int si=0, ei = nums.length-1;
-        while (si < ei) {
-            int sum = nums[si] + nums[ei];
-            if (sum == target) {
-               ans.add(nums[si++]);
-                ans.add(nums[ei--]);
-                return ans; 
+        HashMap<Integer, Integer> numMap = new HashMap<Integer, Integer>();
+
+        for(int i=0; i<nums.length; i++) {
+            int diff = target - nums[i];
+            if (numMap.containsKey(diff)) {
+                result[0] = i;
+                result[1] = numMap.get(diff);
+                return result;
             }
-            else if (sum > target)
-                ei--;
-            else
-                si++;
+            else{
+               numMap.put(nums[i], i); 
+            }
+            
         }
-        
+        return result;
     }
 }
