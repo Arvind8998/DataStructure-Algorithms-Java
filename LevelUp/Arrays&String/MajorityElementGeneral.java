f277710d24389bbe241f26c5b58ada2cd0d2d267
Arvind Sangwan <asangwan8998@gmail.com>
2022-02-08 09:33:20 -0500

    Added changes

--- LevelUp/Arrays&String/MajorityElementGeneral.java
+++ LevelUp/Arrays&String/MajorityElementGeneral.java
@@ -1,18 +1,33 @@
 import java.util.*;
 
 public class Main {
-    
-    //~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~
     public static ArrayList<Integer> majorityElement(int[] arr, int k) {
-        // write yout code here
+
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = arr.length;
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        for (int el : arr) {
+            if (myMap.containsKey(el)) {
+                int freq = myMap.get(el);
+                myMap.put(el, ++freq);
+            } else {
+                myMap.put(el, 1);
+            }
+        }
+
+        myMap.keySet().forEach(el -> {
+            if (myMap.get(el) > n / k) {
+                res.add(el);
+            }
+        });
+        return res;
     }
-    
-    //~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = scn.nextInt();
         }
         int k = scn.nextInt();
