90fd1bd2392de2ed1329ca5ebe99e5f0687c92a6
Arvind Sangwan <asangwan8998@gmail.com>
2022-02-12 00:09:25 -0500

    aDDED MAXCHUNKS PROBLEM

---
+++ LevelUp/Arrays&String/LeetCode769ArrayChunks.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int max = -(int)1e9;
+        int chunks =0;
+        
+        for(int i=0; i<arr.length; i++ ){
+            max = Math.max(max, arr[i]);
+            
+            if(max == i) {
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+}
