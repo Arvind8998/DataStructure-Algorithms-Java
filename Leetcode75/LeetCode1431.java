e59695105f0822c2ee64dc042016da86092d7148
Arvind Sangwan <asangwan8998@gmail.com>
2024-03-29 14:31:57 -0400

    Added changes

---
+++ Leetcode75/LeetCode1431.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+
+        int max= candies[0];
+
+        for(int i=0; i<candies.length; i++){
+            if(candies[i] > max){
+                max = candies[i];
+            }
+        }
+
+        for(int i=0; i<candies.length; i++){
+            ans.add((candies[i] + extraCandies) >= max );
+        }
+        return ans;
+    }
+}
